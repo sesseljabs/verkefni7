37830efb80d203f24f1d8552c7af836a5b72662d
sesseljabs <sesseljabjarney@gmail.com>
2019-11-08 10:12:08 +0000

    langt komin

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, session, url_for, request, redirect, escape
+from flask import Flask, render_template, session, url_for, request, redirect, escape, flash
+from flask import *
 from datetime import datetime
 import pymysql.cursors
 import os
@@ -9,17 +10,16 @@ app.secret_key = os.urandom(16)
 #app.config['SECRET KEY'] = "leyndo"
 
 connection = pymysql.connect(host="tsuts.tskoli.is", user="1609022560", password="mypassword", db="1609022560_verk7", charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
-
 with connection.cursor() as cursor:
     cursor.execute("select * from users where user_name='uwu'")
     result = cursor.fetchall()
     connection.commit()
     #connection.close()
-
         
 
 @app.route("/")
 def index():
+    # user_id = request.cookies.get("uid")
     return render_template("index.html")
 
 @app.route("/login")
@@ -35,19 +35,46 @@ def submit():
         }
     
     with connection.cursor() as cursor:
-        sql = "select * from users where user_name='%s'" % values['username']
-        cursor.execute(sql)
-        user = cursor.fetchone()
-        print(user)
-        connection.close()
+        asql = "select count(*) from users where (user_name='%s' and user_password='%s')" % (values['username'], values["password"])
+        sql = "select (%s) as boolval" % asql
+        print(sql)
+        try:
+            cursor.execute(sql)
+            user = cursor.fetchone()
+        except Exception as e:
+            print(e)
+            return f"login error i guess, {e}"
 
-    if user == None:
-        return "user is none"
+    login = user["boolval"]
+    if login == 0:
+        return "passwd eða lykilorð rangt"
+    elif login == 1:
+        return f"logged in, {values['username']}"
     else:
-        if values["password"] == user["user_password"]:
-            return "worked lol ur now logged in, %s" % user["user_name"]
-        else:
-            return "wrong pass lol"
+        return "sql error"
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/subsignup", methods=["POST"])
+def subsignup():
+    if request.method == "POST":
+        sup = {
+            "username":request.form["username"],
+            "email":request.form["email"],
+            "password":request.form["password"]
+        }
+
+    with connection.cursor() as cursor:
+        sql = "select * from users"
+        cursor.execute(sql)
+        userlist = cursor.fetchall()
+        for i in userlist:
+            pass
+
+    return "lol meme gif"
             
 
 @app.errorhandler(404)
